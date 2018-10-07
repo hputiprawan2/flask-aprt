63f51bb5caa26c7e8d856c7a60db1179d878d279
Winnie <waritchana.r@gmail.com>
2018-10-07 15:49:47 -0700

    Fixed python coding style according to pep8 (pycodestyle) :rabbit:

--- app.py
+++ app.py
@@ -1,63 +1,80 @@
-from flask import Flask,render_template, redirect, url_for, request, session, \
-					flash
+# import the model after import the db
+from models import *
+
+from flask import (
+    flash,
+    Flask,
+    render_template,
+    redirect,
+    request,
+    url_for,
+    session
+)
+from flask_sqlalchemy import SQLAlchemy
+# = from flask.ext.sqlalchemy import SQLAlchemy
 from functools import wraps
-from flask_sqlalchemy import SQLAlchemy # = from flask.ext.sqlalchemy import SQLAlchemy
 # import sqlite3
 
 app = Flask(__name__)
 app.secret_key = "xdfaskjmkd"
 app.database = "sample.db"
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 
 # create the sqlalchemy object
 db = SQLAlchemy(app)
 
-# import the model after import the db
-from models import *
 
 # login required decorator
 def login_required(f):
-	@wraps(f)
-	def wrap(*args, **kwargs):
-		if 'logged_in' in session:
-			return f(*args, **kwargs)
-		else:
-			flash('You need to login.')
-			return redirect(url_for('login'))
-	return wrap		
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login.')
+            return redirect(url_for('login'))
+        return wrap
+
 
 @app.route('/')
 @login_required
 def home():
-	posts = db.session.query(BlogPost).all()
-	return render_template("index.html", posts=posts)
+    posts = db.session.query(BlogPost).all()
+    return render_template("index.html", posts=posts)
+
 
 @app.route('/welcome')
 def welcome():
-	return render_template("welcome.html")
+    return render_template("welcome.html")
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Incorrect Username or Password. Please try again.'
-		else:
-			session['logged_in'] = True
-			flash('You are just logged in')
-			return redirect(url_for('home'))
-	return render_template('login.html', error=error)	
+    error = None
+    if request.method == 'POST':
+        if (
+            request.form['username'] != 'admin' or
+                request.form['password'] != 'admin'
+        ):
+            error = 'Incorrect Username or Password. Please try again.'
+        else:
+            session['logged_in'] = True
+            flash('You are just logged in')
+            return redirect(url_for('home'))
+        return render_template('login.html', error=error)
+
 
 @app.route('/logout')
 @login_required
 def logout():
-	session.pop('logged_in', None)
-	flash('You are just logged out')
-	return redirect(url_for('welcome'))
+    session.pop('logged_in', None)
+    flash('You are just logged out')
+    return redirect(url_for('welcome'))
+
 
 # def connect_db():
-# 	return sqlite3.connect(app.database)	
+#   return sqlite3.connect(app.database)
+
 
 if __name__ == '__main__':
-	app.run(debug=True)	
+    app.run(debug=True)
